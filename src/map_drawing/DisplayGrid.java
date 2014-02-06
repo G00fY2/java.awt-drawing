c40ef194f087e8384eef9a1a5dec8e896b87f0e7
Johannes Jambor <bashundco@web.de>
2014-02-06 21:44:33 +0100

    Cleaning and refactoring

--- src/map_drawing/DisplayGrid.java
+++ src/map_drawing/DisplayGrid.java
@@ -3,37 +3,30 @@ package map_drawing;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.ArrayList;
-
 import javax.swing.JFrame;
-
 import map_making.Field;
 
-
-public class DisplayGrid{
-	
-	
+public class DisplayGrid {
 	public static void displayGrid(int maxSize, int tileSize, ArrayList<ArrayList<Field>> map) {
-
-    JFrame frame = new JFrame("Test");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-    frame.setLayout(new GridLayout(maxSize, maxSize));
-    
-    
-	for(ArrayList<Field> m : map){
-		for(Field field : m)
-		{
-	    	Tile tile = new Tile(tileSize);
-	    	tile.setBackground(field.getColor());
-	        frame.add(tile);
-		}  
-	}   
-
-    frame.pack();
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-    frame.setPreferredSize(new Dimension(tileSize*maxSize, tileSize*maxSize));}
+		JFrame frame = new JFrame("Test");
+		Tile tile; 
+		
+		for(ArrayList<Field> m : map) {
+			for(Field field : m) {
+		    	tile = new Tile(tileSize);
+		    	tile.setBackground(field.getColor());
+		    	
+		        frame.add(tile);
+			}  
+		}   
 	
+	    frame.pack();
+	    frame.setLocationRelativeTo(null);
+	    frame.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+	    frame.setLayout(new GridLayout(maxSize, maxSize));
+	    frame.setPreferredSize(new Dimension(tileSize * maxSize, tileSize * maxSize));
+    }
 }
 
 
--- src/map_drawing/Tile.java
+++ src/map_drawing/Tile.java
@@ -1,18 +1,13 @@
 package map_drawing;
 
 import java.awt.Dimension;
-
 import javax.swing.JPanel;
 
-
-public class Tile extends JPanel{
-	
-
+public class Tile extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
 	public Tile(int tileSize) {
         this.setOpaque(true);
         this.setPreferredSize(new Dimension(tileSize, tileSize));
-	}
-	
+	}	
 }
