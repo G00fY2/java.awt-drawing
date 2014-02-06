e71ac768533e45e90a30ec53ff962f7c732f64c0
Thomas Wirth <thomas-w-@web.de>
2014-02-06 16:58:40 +0100

    added ocean logic

--- src/map_making/MainClass.java
+++ src/map_making/MainClass.java
@@ -16,22 +16,33 @@ public class MainClass {
 		ArrayList<ArrayList<Field>> map = new ArrayList<ArrayList<Field>>();
 		
 
-		for(int i=0; i<maxFieldSize; i++){
+	for(int i=0; i<maxFieldSize; i++){
 			
-			   ArrayList<Field> fieldList = new ArrayList<Field>();  
-			   for(int j=0; j<maxFieldSize; j++){		   
-				   boolean randomBool = random.nextBoolean();
-				   if (randomBool){
-					   fieldList.add(new Land());
-				   }
-				   else{
-					   fieldList.add(new Ocean());
-				   }
-			   }  
-			   
-			   map.add(fieldList);
-   
+		ArrayList<Field> fieldList = new ArrayList<Field>();  
+		for(int j=0; j<maxFieldSize; j++){
+				   
+			if(i==0 || i==maxFieldSize-1){
+				fieldList.add(new Ocean());
 			}
+			else{
+				if(j==0 || j==maxFieldSize-1){
+					fieldList.add(new Ocean());
+				}
+				else{
+					boolean randomBool = random.nextBoolean();
+					if (randomBool){
+						fieldList.add(new Land());
+					}
+					else{
+					fieldList.add(new Ocean());
+					}
+				}
+			}
+		}  
+			   
+		map.add(fieldList);
+   
+	}
 		
 		System.out.println("DEBUG");
 		for(ArrayList<Field> m : map){
