206a64bbb5249381a40899f07c9921a722371089
MrSchneepflug <johannes.jambor@rwth-aachen.de>
2014-02-06 11:36:29 +0100

    Update Field.java

--- src/map_making/Field.java
+++ src/map_making/Field.java
@@ -2,15 +2,14 @@ package map_making;
 
 import java.awt.Color;
 
-
 public abstract class Field {
-
-
-	boolean walkable;
-	int runspeed;
+	private boolean walkable;
+	private int runspeed;
+	private Color color;
+	
+	public Color getColor() {
+		return this.color;
+	}
 	
 	abstract public void test();
-	abstract public Color getColor();
-	
-	
 }
