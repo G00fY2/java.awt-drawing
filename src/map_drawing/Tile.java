beb8feb5932c32b7d084cceec276fcfc146c7554
Thomas <thomas-w-@web.de>
2014-02-06 09:35:47 +0100

    Update Tile.java

--- src/map_drawing/Tile.java
+++ src/map_drawing/Tile.java
@@ -10,7 +10,8 @@ public class Tile extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	
-	public Tile(int tileSize) {
+	public Tile(int tileSize, Field field) {
+	this.setBackground(field.getColor())
         this.setOpaque(true);
         this.setPreferredSize(new Dimension(tileSize, tileSize));
 	}
