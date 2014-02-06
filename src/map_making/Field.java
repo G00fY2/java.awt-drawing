39da76efd203957c11ef56552766c10cc131c0a4
MrSchneepflug <johannes.jambor@rwth-aachen.de>
2014-02-06 11:38:38 +0100

    Update Field.java

--- src/map_making/Field.java
+++ src/map_making/Field.java
@@ -7,6 +7,10 @@ public abstract class Field {
 	private int runspeed;
 	private Color color;
 	
+	public Field(Color color) {
+		this.color = color;
+	}
+	
 	public Color getColor() {
 		return this.color;
 	}
