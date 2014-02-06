1277190be92fb920c6d9ade51ff8ce84c6a2f75d
Thomas Wirth <thomas-w-@web.de>
2014-02-06 13:22:33 +0100

    fixed missing import

--- src/map_drawing/Tile.java
+++ src/map_drawing/Tile.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
+import map_making.Field;
+
 
 public class Tile extends JPanel{
 	
@@ -11,7 +13,7 @@ public class Tile extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
 	public Tile(int tileSize, Field field) {
-	this.setBackground(field.getColor())
+		this.setBackground(field.getColor());
         this.setOpaque(true);
         this.setPreferredSize(new Dimension(tileSize, tileSize));
 	}
