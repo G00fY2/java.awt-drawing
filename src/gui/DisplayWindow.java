ac572af3a92877f8bc0b4383da74b3f609e73fbf
Thomas Wirth <thomas-w-@web.de>
2014-02-19 22:24:18 +0100

    added new class for drawing window

---
+++ src/gui/DisplayWindow.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import map_drawing.IsoView;
+import map_drawing.TwoDView;
+import map_making.Field;
+
+public class DisplayWindow {
+	
+	public static void displayWindow(int space, int maxFields, ArrayList<ArrayList<Field>> map, Boolean grid, int miniscale) {
+		JFrame frame = new JFrame();
+		JPanel container = new JPanel();
+		container.setLayout(new BoxLayout(container,BoxLayout.X_AXIS));
+	
+		JPanel mapPanel = new JPanel();
+		Dimension normalMap = new Dimension(space*2,space*2);
+		mapPanel.setPreferredSize(normalMap);
+		mapPanel.add(new IsoView(space, maxFields, map, grid, 0)).setPreferredSize(normalMap);
+		container.add(mapPanel);
+    
+		// Draw minimap
+		JPanel miniPanel = new JPanel();
+		Dimension miniMap = new Dimension((int)space/miniscale,(int)space/miniscale);
+		miniPanel.setPreferredSize(miniMap);
+		miniPanel.add(new TwoDView(space, maxFields, map, grid, miniscale)).setPreferredSize(miniMap);
+		container.add(miniPanel);
+	
+		frame.add(container);
+		frame.pack();
+		frame.setTitle("Map Generator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);		
+		frame.setVisible(true);
+		frame.setResizable(true);
+	}
+}
