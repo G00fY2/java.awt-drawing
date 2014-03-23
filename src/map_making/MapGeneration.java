040a6a9d6d9d803f5be570fd21fca9a8106a7342
G00fY2 <thomas-w-@web.de>
2014-03-23 19:32:16 +0100

    add simple island generation (atm result is mostly cross-shaped)

--- src/map_making/MapGeneration.java
+++ src/map_making/MapGeneration.java
@@ -9,12 +9,19 @@ public class MapGeneration {
  		
   		ArrayList<ArrayList<Field>> map = new ArrayList<ArrayList<Field>>();
  		Random random = new Random();
-  		
- 		for(int i = 0; i < maxFields; i++){	
+ 		double center = (double) maxFields/2;
+ 		
+ 		for(int i = 0; i < maxFields; i++){
+ 			
+ 			double probI = Math.abs(center-(i<center ? (i==center-0.5 ? i+0.5 : i+1) : i)); //position to be land/water (min = 0 | max = center-1)
+ 			
  			ArrayList<Field> fieldList = new ArrayList<Field>();  
   		
- 			for(int j = 0; j < maxFields; j++) {
- 					fieldList.add(random.nextBoolean() ? new Ocean(tileSize) : new Land(tileSize));
+ 			for(int j = 0; j < maxFields; j++) {			
+ 	 			double probJ = Math.abs(center-(j<center ? (j==center-0.5 ? j+0.5 : j+1) : j)); //position to be land/water (min = 0 | max = center-1)		
+ 	 		
+ 	 			double randomDouble = random.nextDouble()*(center-1);
+ 	 			fieldList.add(randomDouble < probI+probJ ? new Ocean(tileSize) : new Land(tileSize));
  			}
  			
  			map.add(fieldList);
